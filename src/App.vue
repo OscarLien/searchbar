59389ff177d7b4ebdf262903f40a9b1e392c50e3
OscarLien <oscar221243@gmail.com>
2021-01-12 13:22:26 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div class="wrap">
+      <h1>Search Bar</h1>
+      <input type="text"  v-model="input" placeholder="Search"/>
+      <ul class="list" v-for="item in filterSearch" :key="item" :value="item">
+        <li>{{item}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      input: "",
+      list:['Java','JavaScript', 'Python', 'Vue.js','React','Angular'],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+
+  },
+  computed: {
+    filterSearch() {
+      return this.list.filter(searchResult => searchResult.toLowerCase().match(this.input));
+      // return this.list;
+    }
+
+  },
+};
 </script>
 
 <style lang="scss">
@@ -24,5 +46,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.wrap {
+  width: 80%;
+  margin: 0 auto;
+  .list {
+    list-style: none;
+    padding: 0;
+    li {
+      padding: 10px 0;
+    }
+  }
 }
 </style>
